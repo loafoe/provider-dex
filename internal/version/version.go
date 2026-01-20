c34916e65e7a13ef5ec1aec3c67c0f0e2fa9b53d
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2026-01-20 19:29:27 +0100

    Update package metadata

--- internal/version/version.go
+++ internal/version/version.go
@@ -15,4 +15,4 @@ limitations under the License.
 package version
 
 // Version will be overridden with the current version at build time using the -X linker flag
-var Version = "1.0.0"
+var Version = "1.2.0"
